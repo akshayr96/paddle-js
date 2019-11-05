866e8bb43fdae3fe9857e99ab84d9cf2aaf8981e
akshayr96 <akshayrameshbabu@gmail.com>
2019-11-05 15:03:22 +0530

    Added hearts for life

--- scripts/gameEntities/gameState.js
+++ scripts/gameEntities/gameState.js
@@ -1,49 +1,59 @@
 class GameState {
-    constructor(config){
+    constructor(config) {
         this.defaultScore = config.scores.score
         this.defaultLife = config.scores.life
         this.scores = this.defaultScore
         this.life = this.defaultLife
-        this.states =  { PAUSE: 'PAUSE', PLAY: 'PLAY', GAME_OVER: 'GAME_OVER' }
+        this.states = { PAUSE: 'PAUSE', PLAY: 'PLAY', GAME_OVER: 'GAME_OVER' }
         this.state = this.states.PAUSE
-        
-        document.addEventListener('keydown', (event) => {
-            if(event.keyCode == 32){
-                if(this.state == this.states.PLAY) this.state = this.states.PAUSE
-                else if(this.state == this.states.PAUSE) this.state = this.states.PLAY
-                else if(this.state == this.states.GAME_OVER){
-                    this.resetScore()
-                    this.resetLife()
-                    this.state = this.states.PLAY
-                } 
-            }
-        })
+        this.addGameStateShiftListener()
+        this.setScoresToTemplate()
+        this.setLifeToTemplate()
+    }
 
-        document.getElementById("scores").innerHTML = this.scores
-        document.getElementById("life").innerHTML = this.life
-    }
-    
-    incrementScore(){
+    incrementScore() {
         this.scores++
-        document.getElementById("scores").innerHTML = this.scores
+        this.setScoresToTemplate()
     }
-    
-    resetScore(){
+
+    resetScore() {
         this.scores = this.defaultScore
+        this.setScoresToTemplate()
+    }
+
+    setScoresToTemplate() {
         document.getElementById("scores").innerHTML = this.scores
     }
-    
-    decrementLife(){
+
+    decrementLife() {
         this.life = this.life ? this.life - 1 : this.life
-        document.getElementById("life").innerHTML = this.life
-        if(this.life == 0){
+        this.setLifeToTemplate()
+        if (this.life == 0) {
             this.state = this.states.GAME_OVER
         }
     }
 
-    resetLife(){
+    resetLife() {
         this.life = this.defaultLife
-        document.getElementById("life").innerHTML = this.life
+        this.setLifeToTemplate()
+    }
+
+    setLifeToTemplate() {
+        document.getElementById("life").innerHTML = "❤️".repeat(this.life)
+    }
+
+    addGameStateShiftListener(){
+        document.addEventListener('keydown', (event) => {
+            if (event.keyCode == 32) {
+                if (this.state == this.states.PLAY) this.state = this.states.PAUSE
+                else if (this.state == this.states.PAUSE) this.state = this.states.PLAY
+                else if (this.state == this.states.GAME_OVER) {
+                    this.resetScore()
+                    this.resetLife()
+                    this.state = this.states.PLAY
+                }
+            }
+        })
     }
 }
 
